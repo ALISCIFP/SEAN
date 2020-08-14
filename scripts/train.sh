9a1011dde07e6e567e43d932c9a9e6d9982d0571
Zengming Shen <zengming.shen@jd.com>
2020-08-14 12:06:36 +0800

    add scr

---
+++ scripts/train.sh
@@ -0,0 +1,1 @@
+python train.py --continue_train --name CelebA-HQ --load_size 256 --crop_size 256 --dataset_mode custom --label_dir datasets/CelebA-HQ/train/labels --image_dir datasets/CelebA-HQ/train/images --label_nc 19 --no_instance --batchSize 21 --gpu_ids 1,2,3
